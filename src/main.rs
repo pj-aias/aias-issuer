5e1ed817b6bf47506a1c808ac5b8fb9890f0b17a
akakou <akakou571@gmail.com>
2021-08-15 19:58:59 +0900

    [add] Initial actix server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use actix_session::CookieSession;
+use actix_web::{web, App, HttpServer};
+
+mod handler;
+use std::io;
+
+#[actix_rt::main]
+async fn main() -> io::Result<()> {
+    HttpServer::new(move || {
+        App::new()
+            .wrap(CookieSession::signed(&[0; 32]).secure(true))
+            // .data(data.clone())
+            .route("/hello", web::get().to(handler::hello))
+    })
+    .bind("0.0.0.0:8080")?
+    .run()
+    .await
 }
