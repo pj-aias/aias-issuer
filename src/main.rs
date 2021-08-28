239b3b3bd4bf61210e54757fd9630623efe965e7
akakou <akakou571@gmail.com>
2021-08-28 20:34:13 +0900

    [remove] Issueing credential

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,6 @@ extern crate rbatis;
 
 use actix_session::CookieSession;
 use actix_web::{web, App, HttpServer};
-use std::sync::Arc;
-use std::sync::Mutex;
 
 use rand::Rng;
 
@@ -24,20 +22,14 @@ async fn main() -> io::Result<()> {
         key[i] = rng.gen();
     }
 
-    let rb = db::init_db();
-    let data = Arc::new(Mutex::new(rb));
+    let _ = db::init_db();
 
     HttpServer::new(move || {
         App::new()
             .wrap(CookieSession::private(&key).secure(true))
-            .data(data.clone())
             .route("/hello", web::get().to(handler::hello))
             .route("/send_code", web::post().to(handler::send_code))
             .route("/verify_code", web::post().to(handler::verify_code))
-            .route(
-                "/issue_credential",
-                web::post().to(handler::issue_credential),
-            )
     })
     .bind("0.0.0.0:8080")?
     .run()
